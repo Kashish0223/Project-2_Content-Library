d1aa19c4d82bcd36fc72cb033665b45814324ad3
Kashish0223 <153355361+Kashish0223@users.noreply.github.com>
2024-08-05 22:46:18 +0530

    Update Databse_Path app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from waitress import serve
 app = Flask(__name__)
 app.secret_key = os.environ.get('SECRET_KEY', 'your_default_secret_key')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'postgresql://library_sql_user:SVhrlpx5HWu17QBUvJAucjTmZgha78ZP@dpg-cqlrv3ggph6c738lman0-a.oregon-postgres.render.com/library_sql')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'postgresql://library_p9o0_user:FBeVgX8OZGLENBqUzW7bzj5xR1910WC8@dpg-cqnq063v2p9s73agp110-a.oregon-postgres.render.com/library_p9o0')
 # postgresql://library_sql_user:SVhrlpx5HWu17QBUvJAucjTmZgha78ZP@dpg-cqlrv3ggph6c738lman0-a.oregon-postgres.render.com/library_sql
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 database.init_app(app)
